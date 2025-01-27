7d971ebf43c0fbf670b9c02837f6333a0d8c67e1
JoepieSnels <j.snels@student.avans.nl>
2025-01-28 00:24:08 +0100

    maybe now

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8250d37010435a046c96ff5dab6bf6d2399c740")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa1cc0237f1e85ffad7da1a3f33f5c28d5697452")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
